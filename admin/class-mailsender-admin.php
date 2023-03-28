e2f040b424ef4e735c9676e5b0b9a2f1795fce09
mukesh <mukesh.choudhari@wisdmlabs.com>
2023-03-28 11:22:25 +0530

    Segregated code in different files

--- admin/class-mailsender-admin.php
+++ admin/class-mailsender-admin.php
@@ -20,7 +20,8 @@
  * @subpackage Mailsender/admin
  * @author     Mukesh <mukesh@mukesh.com>
  */
-class Mailsender_Admin {
+class Mailsender_Admin
+{
 
 	/**
 	 * The ID of this plugin.
@@ -47,11 +48,11 @@ class Mailsender_Admin {
 	 * @param      string    $plugin_name       The name of this plugin.
 	 * @param      string    $version    The version of this plugin.
 	 */
-	public function __construct( $plugin_name, $version ) {
+	public function __construct($plugin_name, $version)
+	{
 
 		$this->plugin_name = $plugin_name;
 		$this->version = $version;
-
 	}
 
 	/**
@@ -59,7 +60,8 @@ class Mailsender_Admin {
 	 *
 	 * @since    1.0.0
 	 */
-	public function enqueue_styles() {
+	public function enqueue_styles()
+	{
 
 		/**
 		 * This function is provided for demonstration purposes only.
@@ -73,8 +75,7 @@ class Mailsender_Admin {
 		 * class.
 		 */
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/mailsender-admin.css', array(), $this->version, 'all' );
-
+		wp_enqueue_style($this->plugin_name, plugin_dir_url(__FILE__) . 'css/mailsender-admin.css', array(), $this->version, 'all');
 	}
 
 	/**
@@ -82,7 +83,8 @@ class Mailsender_Admin {
 	 *
 	 * @since    1.0.0
 	 */
-	public function enqueue_scripts() {
+	public function enqueue_scripts()
+	{
 
 		/**
 		 * This function is provided for demonstration purposes only.
@@ -96,9 +98,111 @@ class Mailsender_Admin {
 		 * class.
 		 */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/mailsender-admin.js', array( 'jquery' ), $this->version, false );
+		wp_enqueue_script($this->plugin_name, plugin_dir_url(__FILE__) . 'js/mailsender-admin.js', array('jquery'), $this->version, false);
 
-		
+		//Made for Trial of Sending Mail Every Minute
+		if (!function_exists('md_add_cron_schedules')) :
+			function md_add_cron_schedules($schedules = array())
+			{
+
+				$schedules['every_minute'] = array(
+					'interval' => 120,
+					'display' => __('Every Minute', 'maildesk'),
+				);
+				return $schedules;
+			}
+			add_filter('cron_schedules', 'md_add_cron_schedules');
+		endif;
+
+
+		function schedule_daily_post_summary()
+		{
+			if (!wp_next_scheduled('send_daily_post_summary')) {
+				wp_schedule_event(time(), 'every_minute', 'send_daily_post_summary');
+			}
+		}
+
+
+
+
+		function get_daily_post_summary()
+		{
+			$args = array(
+				'date_query' => array(
+					array(
+						'after' => '24 hours ago',
+					),
+				),
+			);
+			$query = new WP_Query($args);
+			$posts = $query->posts;
+			$summary = array();
+
+			foreach ($posts as $post) {
+				$post_data = array(
+					'title' => $post->post_title,
+					'url' => get_permalink($post->ID),
+					'meta_title' => get_post_meta($post->ID, '_yoast_wpseo_title', true),
+					'meta_description' => get_post_meta($post->ID, '_yoast_wpseo_metadesc', true),
+					'meta_keywords' => get_post_meta($post->ID, '_yoast_wpseo_focuskw', true),
+					'page_speed' => get_page_speed_score(get_permalink($post->ID)),
+				);
+				array_push($summary, $post_data);
+			}
+
+			return $summary;
+		}
+
+
+		function send_daily_post_summary_callback()
+		{
+			$to = get_option('admin_email');
+			$subject = 'Daily Post Summary';
+			$summary = get_daily_post_summary();
+			$message = '';
+
+			foreach ($summary as $post_data) {
+				$message .= 'Title: ' . $post_data['title'] . "\n";
+				$message .= 'URL: ' . $post_data['url'] . "\n";
+				$message .= 'Meta Title: ' . $post_data['meta_title'] . "\n";
+				$message .= 'Meta Description: ' . $post_data['meta_description'] . "\n";
+				$message .= 'Meta Keywords: ' . $post_data['meta_keywords'] . "\n";
+				$message .= 'Page Speed Score: ' . $post_data['page_speed'] . " seconds \n";
+				$message .= "\n";
+			}
+			$headers = array(
+				'From: mukesh.choudhari@wisdmlabs.com',
+				'Content-Type: text/html; charset=UTF-8'
+			);
+
+			wp_mail($to, $subject, $message, $headers);
+		}
+		//Google Page Speed
+		function get_page_speed_score($url)
+		{
+
+			$api_key = "416ca0ef-63e4-4caa-a047-ead672ecc874"; // your api key
+			$new_url = "http://www.webpagetest.org/runtest.php?url=" . $url . "&runs=1&f=xml&k=" . $api_key;
+			$run_result = simplexml_load_file($new_url);
+			$status = $run_result->statusCode;
+			if ($status == 400) {
+				$error = "API Limit Crossed!";
+				return $error;
+			} else {
+				$test_id = $run_result->data->testId;
+
+				$status_code = 100;
+
+				while ($status_code != 200) {
+					sleep(10);
+					$xml_result = "http://www.webpagetest.org/xmlResult/" . $test_id . "/";
+					$result = simplexml_load_file($xml_result);
+					$status_code = $result->statusCode;
+					$time = (float) ($result->data->median->firstView->loadTime) / 1000;
+				};
+
+				return $time;
+			}
+		}
 	}
-
 }
